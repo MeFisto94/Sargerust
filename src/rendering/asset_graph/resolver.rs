4937517a40af3edfd1047ae4f74b4ace50af67eb
MeFisto94 <MeFisto94@users.noreply.github.com>
2023-10-14 23:48:55 +0200

    Resolver: Switch to dashmap to greatly reduce locking time

--- src/rendering/asset_graph/resolver.rs
+++ src/rendering/asset_graph/resolver.rs
@@ -1,9 +1,9 @@
-use std::collections::HashMap;
+use dashmap::DashMap;
 use std::ops::DerefMut;
 use std::sync::{Arc, RwLock, RwLockWriteGuard, Weak};
 
 pub struct Resolver<G: GraphNodeGenerator<T>, T> {
-    ref_cache: RwLock<HashMap<String, RwLock<Weak<T>>>>,
+    ref_cache: DashMap<String, RwLock<Weak<T>>>,
     generator: G,
 }
 
@@ -14,21 +14,22 @@ pub trait GraphNodeGenerator<T> {
 impl<G: GraphNodeGenerator<T>, T> Resolver<G, T> {
     pub fn new(generator: G) -> Self {
         Self {
-            ref_cache: RwLock::new(HashMap::new()),
+            ref_cache: DashMap::with_capacity(100),
             generator,
         }
     }
 
-    // TODO: maybe take name by reference and only own it when inserting
-    // TODO: also canonicalize paths: uppercase and forward slashes as in MPQ?
+    // TODO: maybe take name by reference and only own it when inserting.
+    //  also canonicalize paths: uppercase and forward slashes as in MPQ?
+    //  -> Those two requirements do conflict, though.
     pub fn resolve(&self, name: String) -> Arc<T> {
+        // TODO: This is one of the hottest paths when loading assets, due to the locking on the hashmap
+        //  consider experimenting with alternatives (chashmap, dashmap, leapfrog). However,
+        //  according to the benchmark from the dashmap author (https://github.com/xacrimon/conc-map-bench)
+        //  dashmap should be very competetive and outperform chashmap.
         // Easy path: The cache contains a weak reference
-        if let Some(weak_lock) = self
-            .ref_cache
-            .read()
-            .expect("Get the read lock on the cache")
-            .get(&name)
-        {
+        if let Some(weak_lock) = self.ref_cache.get(&name) {
+            // TODO: with dashmap, could we also just get_mut instead of having RwLocks inside the entries?
             {
                 let weak = weak_lock.read().expect("Get the read lock on the entry");
                 if let Some(arc) = weak.upgrade() {
@@ -43,24 +44,16 @@ impl<G: GraphNodeGenerator<T>, T> Resolver<G, T> {
 
         // Heavier path: We need to lock the entire cache to insert a new weak reference, however
         // we try to do this as short as possible by _not_ waiting for the generator.
+        // Note: the above comment is kind-of outdated with DashMap but was true for RwLock<HashMap<_>>
+        // the reasoning still remains, though, as we just lock on smaller buckets but still do lock.
         {
-            let mut wlock = self
-                .ref_cache
-                .write()
-                .expect("Get the write lock on the cache");
-            wlock.insert(name.clone(), RwLock::new(Weak::new()));
+            self.ref_cache
+                .insert(name.clone(), RwLock::new(Weak::new()));
         }
 
         {
-            let rlock = self
-                .ref_cache
-                .read()
-                .expect("Get the read lock on the cache");
-            let mut weak = rlock
-                .get(&name)
-                .unwrap()
-                .write()
-                .expect("Get the write lock on the entry");
+            let entry = self.ref_cache.get(&name).unwrap();
+            let mut weak = entry.write().expect("Get the write lock on the entry");
             Self::generate(self, &name, &mut weak)
         }
     }
