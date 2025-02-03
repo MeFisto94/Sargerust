763889ab7d05432c4bbafd8c27ac8d08d5fa159d
MeFisto94 <MeFisto94@users.noreply.github.com>
2025-02-03 01:02:40 +0100

    MPQ: Fix compiler warning

--- mpq-rust/src/lib.rs
+++ mpq-rust/src/lib.rs
@@ -1,6 +1,6 @@
 //! A library for reading MPQ archives
 
-#![cfg_attr(feature = "cargo-clippy", allow(clippy::unreadable_literal))]
+#![allow(clippy::unreadable_literal)]
 
 mod archive;
 mod chain;
